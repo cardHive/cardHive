d8c0d2a7eb15819d1b0bb2b1da2929c5ee2f01b2
Matthias Alt <mattagohni@gmail.com>
2020-06-01 13:56:55 +0200

    initial commit

---
+++ src/main/java/de/mattaghoni/cardhive/CardhiveApplication.java
@@ -0,0 +1,13 @@
+package de.mattaghoni.cardhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardhiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardhiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/mattaghoni/cardhive/CardhiveApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mattaghoni.cardhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardhiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
