10c5f7dfca85e2199c904e2f7319af635cd7ea44
Matthias Alt <mattagohni@gmail.com>
2020-06-07 17:01:51 +0200

    add angular application with nx-cli

---
+++ card-hive/apps/card-hive/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'card-hive-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.styl'],
+})
+export class AppComponent {
+  title = 'card-hive';
+}
