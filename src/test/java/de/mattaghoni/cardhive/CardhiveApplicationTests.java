1fe6717f4ec04605e58e3c825700c682b4ee6707
Matthias Alt <mattagohni@gmail.com>
2020-06-07 15:38:02 +0200

    #1: add test for springboot application to satisfy sonarcloud

---
+++ src/test/java/de/mattaghoni/cardhive/CardhiveApplicationTests.java
@@ -0,0 +1,12 @@
+package de.mattaghoni.cardhive;
+
+import org.junit.jupiter.api.Test;
+
+class CardhiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Test only exists to satisfy sonarcloud.
+		CardhiveApplication.main(new String[]{});
+	}
+}
