d4099c8f4e92c577567dcbdb173977fc7cc5df16
Matthias Alt <mattagohni@gmail.com>
2020-06-07 15:26:59 +0200

    #1: add mehtod to repository to find Games by its name

--- src/test/groovy/de/mattaghoni/cardhive/domain/repository/GameRepositorySpec.groovy
+++ src/test/groovy/de/mattaghoni/cardhive/domain/repository/GameRepositorySpec.groovy
@@ -3,6 +3,7 @@ package de.mattaghoni.cardhive.domain.repository
 import de.mattaghoni.cardhive.domain.entity.Game
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import reactor.test.StepVerifier
 import spock.lang.Specification
 
 @DataJpaTest
@@ -24,5 +25,17 @@ class GameRepositorySpec extends Specification {
 
         then: "the game has a database id"
         result.getId() != null
+    }
+
+    def "it can find a game by its name" () {
+        given: "there is a game in the database"
+        Game game = new Game(null, "Slay the Spire")
+        def expected = gameRepository.saveAndFlush(game)
+
+        when: "we load this game from the database"
+        def actual = gameRepository.findByName("Slay the Spire").orElseThrow()
+
+        then: "we get the correct game"
+        actual == expected
     }
 }
